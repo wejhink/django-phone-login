e89a1d02d46a02f12d25aca9e407e9aa2a2c1f99
Ray Ch <i@iraycd.com>
2016-08-04 19:33:22 +0530

    Changes in requirements and setup.py

--- setup.py
+++ setup.py
@@ -28,87 +28,38 @@ standard_exclude_directories = [
 ]
 
 
-# Copied from paste/util/finddata.py
-def find_package_data(where=".", package="", exclude=standard_exclude,
-                      exclude_directories=standard_exclude_directories,
-                      only_in_packages=True, show_ignored=False):
+def get_packages(package):
     """
-    Return a dictionary suitable for use in ``package_data``
-    in a distutils ``setup.py`` file.
-    The dictionary looks like::
-        {"package": [files]}
-    Where ``files`` is a list of all the files in that package that
-    don't match anything in ``exclude``.
-    If ``only_in_packages`` is true, then top-level directories that
-    are not packages won't be included (but directories under packages
-    will).
-    Directories matching any pattern in ``exclude_directories`` will
-    be ignored; by default directories with leading ``.``, ``CVS``,
-    and ``_darcs`` will be ignored.
-    If ``show_ignored`` is true, then all the files that aren't
-    included in package data are shown on stderr (for debugging
-    purposes).
-    Note patterns use wildcards, or can be exact paths (including
-    leading ``./``), and all searching is case-insensitive.
+    Return root package and all sub-packages.
     """
-
-    out = {}
-    stack = [(convert_path(where), "", package, only_in_packages)]
-    while stack:
-        where, prefix, package, only_in_packages = stack.pop(0)
-        for name in os.listdir(where):
-            fn = os.path.join(where, name)
-            if os.path.isdir(fn):
-                bad_name = False
-                for pattern in exclude_directories:
-                    if (fnmatchcase(name, pattern)
-                            or fn.lower() == pattern.lower()):
-                        bad_name = True
-                        if show_ignored:
-                            print >> sys.stderr, (
-                                "Directory %s ignored by pattern %s"
-                                % (fn, pattern))
-                        break
-                if bad_name:
-                    continue
-                if (os.path.isfile(os.path.join(fn, "__init__.py"))
-                        and not prefix):
-                    if not package:
-                        new_package = name
-                    else:
-                        new_package = package + "." + name
-                    stack.append((fn, "", new_package, False))
-                else:
-                    stack.append((fn, prefix + name + "/", package,
-                                  only_in_packages))
-            elif package or not only_in_packages:
-                # is a file
-                bad_name = False
-                for pattern in exclude:
-                    if (fnmatchcase(name, pattern)
-                            or fn.lower() == pattern.lower()):
-                        bad_name = True
-                        if show_ignored:
-                            print >> sys.stderr, (
-                                "File %s ignored by pattern %s"
-                                % (fn, pattern))
-                        break
-                if bad_name:
-                    continue
-                out.setdefault(package, []).append(prefix+name)
-    return out
+    return [dirpath
+            for dirpath, dirnames, filenames in os.walk(package)
+            if os.path.exists(os.path.join(dirpath, '__init__.py'))]
 
 
-excluded_directories = standard_exclude_directories
+def get_package_data(package):
+    """
+    Return all files under the root package, that are not in a
+    package themselves.
+    """
+    walk = [(dirpath.replace(package + os.sep, '', 1), filenames)
+            for dirpath, dirnames, filenames in os.walk(package)
+            if not os.path.exists(os.path.join(dirpath, '__init__.py'))]
 
-package_data = find_package_data(exclude_directories=excluded_directories)
+    filepaths = []
+    for base, filenames in walk:
+        filepaths.extend([os.path.join(base, filename)
+                          for filename in filenames])
+    return {package: filepaths}
+
+REQUIREMENTS = [i.strip() for i in open("requirements.txt").readlines()]
 
 # allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-phone-login',
-    version='0.1.1',
+    version='0.1.2',
     include_package_data=True,
     license='BSD License',
     description='Django Phone Login: Login with OTP',
@@ -116,6 +67,7 @@ setup(
     url='https://github.com/gonnamake/django-phone-login',
     author='Ray Ch',
     author_email='ray@jhink.com',
+    install_requires=REQUIREMENTS,
     classifiers=[
         'Environment :: Web Environment',
         'Framework :: Django',
@@ -129,6 +81,6 @@ setup(
         'Topic :: Internet :: WWW/HTTP',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
     ],
-    packages=find_packages(exclude=['example']),
-    package_data=package_data,
+    packages=get_packages('phone_login'),
+    package_data=get_package_data('phone_login'),
 )
