f081dfa784d537d484d3219e15ed804acfebf498
Ray Ch <i@iraycd.com>
2016-09-02 02:48:43 +0530

    Username will be set to UUID

--- phone_login/backends/phone_backend.py
+++ phone_login/backends/phone_backend.py
@@ -1,12 +1,27 @@
 import datetime
+import uuid
 
 from django.conf import settings
 from django.contrib.auth import get_user_model, login
 from django.contrib.auth.backends import ModelBackend
 
+from ..utils import model_field_attr
 from ..models import PhoneToken
 
 class PhoneBackend(ModelBackend):
+
+
+    def __init__(self, *args, **kwargs):
+        self.user_model = get_user_model()
+
+    def get_username(self):
+        """
+        Returns a UUID-based 'random' and unique username.
+
+        This is required data for user models with a username field.
+        """
+        return str(uuid.uuid4())[:model_field_attr(self.user_model, 'username', 'max_length')]
+
     def authenticate(self, otp=None, **credentials):
         phone_number = credentials.get("phone_number")
         phone_token = PhoneToken.objects.filter(
@@ -20,7 +35,7 @@ class PhoneBackend(ModelBackend):
             if not user:
                 password = User.objects.make_random_password()
                 user = User.objects.create_user(
-                    username=phone_number,
+                    username=self.get_username(),
                     password=password,
                     email="",
                     phone_number=phone_number
--- phone_login/utils.py
+++ phone_login/utils.py
@@ -34,3 +34,11 @@ def user_detail(user, last_login):
         "status": status.HTTP_200_OK
     }
     return (USER, STATUS["status"])
+
+
+def model_field_attr(model, model_field, attr):
+    """
+    Returns the specified attribute for the specified field on the model class.
+    """
+    fields = dict([(field.name, field) for field in model._meta.fields])
+    return getattr(fields[model_field], attr)
