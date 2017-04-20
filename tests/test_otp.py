b19b18ee20d58421b3825a425b5aa4a29b1d7409
Ray Ch <i@iraycd.com>
2017-04-20 16:06:29 +0530

    Login Attempts Time Test

--- tests/test_otp.py
+++ tests/test_otp.py
@@ -8,6 +8,9 @@
 # 5. Failure - When OTP is not given.
 from phone_login.models import PhoneToken
 from django.test import TransactionTestCase
+from django.conf import settings
+import datetime
+
 
 
 class PhoneTokenTest(TransactionTestCase):
@@ -26,5 +29,11 @@ class PhoneTokenTest(TransactionTestCase):
     def test_create_otp_for_number(self):
         phone_number = "+01 9212128291"
         phone_token = self.phone_token.create_otp_for_number(phone_number)
+
+        today_min = datetime.datetime.combine(datetime.date.today(), datetime.time.min)
+        today_max = datetime.datetime.combine(datetime.date.today(), datetime.time.max)
+
+        otps = self.phone_token.objects.filter(phone_number=phone_number, timestamp__range=(today_min, today_max))
+        self.assertTrue(otps.count() <= getattr(settings, 'PHONE_LOGIN_ATTEMPTS', 10), 'message')
         self.assertIsNotNone(phone_token, 'Test Created OTP')
         self.assertIsNotNone(phone_token.otp, 'OTP Exists')
